08596052bd1d618502de76ee08d9354ca8a6b464
branyerbjr <branyer.romero@tecsup.edu.pe>
2023-06-25 11:46:31 -0500

    comenzamos la primera estructura

---
+++ src/components/LoginComp.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="mb-4">
+        <div class="card">
+            <div class="card-header">
+                <h1>Login</h1>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/views/IndexView.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="text-center">
+        <h1>INDEX</h1>
+    </div>
+</template>
+<script>
+</script>
+<style>
+
+</style>
