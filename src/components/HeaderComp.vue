24a37a63785b21b60870071991871c02c92f3bf2
branyerbjr <branyer.romero@tecsup.edu.pe>
2023-06-30 16:59:18 -0500

    acomplando funcionalidades

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,21 +1,43 @@
 <template>
-  <nav class="navbar navbar-expand-md navbar-white fixed-top bg-white"  style="box-shadow: 0 4px 5px #e5e5e5;">
+  <nav
+    class="navbar navbar-expand-md navbar-white fixed-top bg-white"
+    style="box-shadow: 0 4px 5px #e5e5e5"
+  >
     <div class="container-fluid">
-      <router-link :to="{ name:'index'}">
+      <router-link :to="{ name: 'index' }">
         <a class="navbar-brand" href="#">
-        <img src="../assets/img/fithub.png" alt="Logo FitHub" style="height: 25px;">
-      </a>
+          <img
+            src="../assets/img/fithub.png"
+            alt="Logo FitHub"
+            style="height: 25px"
+          />
+        </a>
       </router-link>
-      
+
       <ul class="nav nav-masthead justify-content-end nav-separator">
         <li class="nav-item">
-          <router-link :to="{ name:'login'}">
-            <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Iniciar sesión</a>
+          <router-link :to="{ name: 'login' }">
+            <a
+              :class="{
+                'nav-link fw-bold py-1 px-0 ': true,
+                active: isRouteActive('login'),
+              }"
+              aria-current="page"
+              href="#"
+              >Iniciar sesión</a
+            >
           </router-link>
         </li>
         <li class="nav-item">
-          <router-link :to="{ name:'register'}">
-            <a class="nav-link fw-bold py-1 px-0" href="#">Registrarse</a>
+          <router-link :to="{ name: 'register' }">
+            <a
+              :class="{
+                'nav-link fw-bold py-1 px-0 ': true,
+                active: isRouteActive('register'),
+              }"
+              href="#"
+              >Registrarse</a
+            >
           </router-link>
         </li>
       </ul>
@@ -24,10 +46,15 @@
 </template>
 <script>
 export default {
-  name: "Header"
-  }
+  name: "Header",
+  methods: {
+    isRouteActive(routeName) {
+      return this.$route.name === routeName;
+    },
+  },
+};
 </script>
 <style>
 @import "@/assets/css/navbar.css";
-@import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@500&family=Nunito:wght@500;700;800&family=Poppins:wght@300&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Comfortaa:wght@500&family=Nunito:wght@500;700;800&family=Poppins:wght@300&display=swap");
 </style>
