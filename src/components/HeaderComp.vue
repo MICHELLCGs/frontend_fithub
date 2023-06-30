c2e487bdf5d40d1d164733863155955d515bb6aa
branyerbjr <branyer.romero@tecsup.edu.pe>
2023-06-30 10:16:39 -0500

    haciendo cambios

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,17 +1,22 @@
 <template>
   <nav class="navbar navbar-expand-md navbar-white fixed-top bg-white"  style="box-shadow: 0 4px 5px #e5e5e5;">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">
+      <router-link :to="{ name:'index'}">
+        <a class="navbar-brand" href="#">
         <img src="../assets/img/fithub.png" alt="Logo FitHub" style="height: 25px;">
       </a>
+      </router-link>
+      
       <ul class="nav nav-masthead justify-content-end nav-separator">
         <li class="nav-item">
-          <router-link :to="{ name:''}">
+          <router-link :to="{ name:'login'}">
             <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Iniciar sesión</a>
           </router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link fw-bold py-1 px-0" href="#">Registrarse</a>
+          <router-link :to="{ name:'register'}">
+            <a class="nav-link fw-bold py-1 px-0" href="#">Registrarse</a>
+          </router-link>
         </li>
       </ul>
     </div>
---
+++ src/components/layout/sidebars/SidebarAdmin.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- Sidebar -->
+  <div class="col-md-3 bg-dark text-white">
+    <div class="sidebar">
+      <h2>Dashboard</h2>
+      <ul class="list-unstyled">
+        <li><a href="#">Inicio</a></li>
+        <li>
+          <a
+            href="#submenu"
+            data-bs-toggle="collapse"
+            role="button"
+            aria-expanded="false"
+            aria-controls="submenu"
+            >Menú <i class="bi bi-chevron-down"></i
+          ></a>
+          <ul class="collapse" id="submenu">
+            <li><a href="#">Opción 1</a></li>
+            <li><a href="#">Opción 2</a></li>
+            <li><a href="#">Opción 3</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Configuración</a></li>
+      </ul>
+    </div>
+    </div>
+    <slot></slot>
+  <!-- Contenido principal -->
+</template>
+<script>
+export default {
+  name: "SidebarAdmin",
+};
+</script>
