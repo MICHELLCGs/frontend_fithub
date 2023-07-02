d35b95be2fca7c1cac05730d1754128565db42ae
Michell Condori <michell.condori@tecsup.edu.pe>
2023-07-02 18:29:25 -0500

    Implementación_sidebar

--- src/components/layout/sidebars/SidebarAdmin.vue
+++ src/components/layout/sidebars/SidebarAdmin.vue
@@ -1,34 +1,256 @@
 <template>
-  <!-- Sidebar -->
-  <div class="col-md-3 bg-dark text-white">
-    <div class="sidebar">
-      <h2>Dashboard</h2>
-      <ul class="list-unstyled">
-        <li><a href="#">Inicio</a></li>
-        <li>
-          <a
-            href="#submenu"
-            data-bs-toggle="collapse"
-            role="button"
-            aria-expanded="false"
-            aria-controls="submenu"
-            >Menú <i class="bi bi-chevron-down"></i
-          ></a>
-          <ul class="collapse" id="submenu">
-            <li><a href="#">Opción 1</a></li>
-            <li><a href="#">Opción 2</a></li>
-            <li><a href="#">Opción 3</a></li>
-          </ul>
+  <div class="wrapper">
+    <!-- Sidebar  -->
+    <nav id="sidebar" :class="{ active: sidebarActive }">
+      <div class="sidebar-header">
+        <h3>ADMIN</h3>
+
+      </div>
+
+      <ul class="list-unstyled components">
+        <p>PERFIL</p>
+        <li >
+          <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false"
+            >Home</a
+          >
         </li>
-        <li><a href="#">Configuración</a></li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false"
+            >Pages</a
+          >
+        </li>
+        <li>
+          <a href="#">Portfolio</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
       </ul>
+    </nav>
+
+    <!-- Page Content  -->
+    <div id="content">
+      <nav >
+        <div class="container-fluid">
+          <button type="button" id="sidebarCollapse" class="btn btn-dark">
+            <i class="fas fa-align-left"></i>
+            <span><i class="bi bi-justify"></i></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="nav navbar-nav ml-auto">
+
+              <li class="nav-item">
+
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
-    </div>
-    <slot></slot>
-  <!-- Contenido principal -->
+  </div>
 </template>
 <script>
+import $ from "jquery";
 export default {
   name: "SidebarAdmin",
+  mounted() {
+    $(document).ready(function () {
+      $("#sidebarCollapse").on("click", function () {
+        $("#sidebar").toggleClass("active");
+      });
+    });
+  },
 };
 </script>
+
+<style>
+@import "https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700";
+body {
+  font-family: "Poppins", sans-serif;
+  background: #fafafa;
+}
+
+p {
+  font-family: "Poppins", sans-serif;
+  font-size: 1.1em;
+  font-weight: 300;
+  line-height: 1.7em;
+  color: #999;
+}
+
+a,
+a:hover,
+a:focus {
+  color: inherit;
+  text-decoration: none;
+  transition: all 0.3s;
+}
+
+.navbar {
+  padding: 15px 10px;
+  background: #fff;
+  border: none;
+  border-radius: 0;
+  margin-bottom: 40px;
+  box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.1);
+}
+
+.navbar-btn {
+  box-shadow: none;
+  outline: none !important;
+  border: none;
+}
+
+.line {
+  width: 100%;
+  height: 1px;
+  border-bottom: 1px dashed #ddd;
+  margin: 40px 0;
+}
+
+/* ---------------------------------------------------
+    SIDEBAR STYLE
+----------------------------------------------------- */
+
+.wrapper {
+  display: flex;
+  width: 15%;
+  align-items: stretch;
+}
+
+#sidebar {
+  /* min-width: 250px;
+  max-width: 250px;
+  background: #7386d5;
+  color: #fff;
+  transition: all 0.3s; */
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 250px;
+  background: #464956;
+  color: #fff;
+  transition: all 0.3s;
+  overflow: hidden;
+}
+
+#sidebar.active {
+  margin-left: -250px;
+}
+
+#sidebar .sidebar-header {
+  padding: 20px;
+  background: #464956;
+}
+
+#sidebar ul.components {
+  padding: 20px 0;
+  border-bottom: 1px solid #464956;
+}
+
+#sidebar ul p {
+  color: #fff;
+  padding: 10px;
+}
+
+#sidebar ul li a {
+  padding: 10px;
+  font-size: 1.1em;
+  display: block;
+}
+
+#sidebar ul li a:hover {
+  color: #7386d5;
+  background: #fff;
+}
+.sidebar-content {
+  padding-top: 20px;
+  position: relative;
+}
+#sidebar ul li.active > a,
+a[aria-expanded="true"] {
+  color: #fff;
+  background: #6d7fcc;
+}
+
+a[data-toggle="collapse"] {
+  position: relative;
+}
+
+.dropdown-toggle::after {
+  display: block;
+  position: absolute;
+  top: 50%;
+  right: 20px;
+  transform: translateY(-50%);
+}
+
+ul ul a {
+  font-size: 0.9em !important;
+  padding-left: 30px !important;
+  background: #6d7fcc;
+}
+
+ul.CTAs {
+  padding: 20px;
+}
+
+ul.CTAs a {
+  text-align: center;
+  font-size: 0.9em !important;
+  display: block;
+  border-radius: 5px;
+  margin-bottom: 5px;
+}
+
+a.download {
+  background: #fff;
+  color: #7386d5;
+}
+
+a.article,
+a.article:hover {
+  background: #6d7fcc !important;
+  color: #fff !important;
+}
+
+/* ---------------------------------------------------
+    CONTENT STYLE
+----------------------------------------------------- */
+
+#content {
+  /* width: 100%;
+  padding: 20px;
+  min-height: 100vh;
+  transition: all 0.3s; */
+  margin-left: 100px;
+  padding: 20px;
+  transition: all 0.3s;
+  margin-top: -32px; 
+}
+#sidebar.active {
+  margin-left: -250px;
+}
+
+
+/* ---------------------------------------------------
+    MEDIAQUERIES
+----------------------------------------------------- */
+
+@media (max-width: 768px) {
+  #sidebar {
+    margin-left: -250px;
+  }
+  #sidebar.active {
+    margin-left: 0;
+  }
+  #sidebarCollapse span {
+    display: none;
+  }
+}
+</style>
--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,183 +1,277 @@
 <template>
-  <div>
-    <HeaderAdmin />
-  </div>
-  <div class="content">
-    <div class="tab">
-      <section class="attendance">
-        <div class="attendance-list table-container">
-          <section class="attendance">
-            <div class="attendance-list">
-              <h1>Tabla Usuarios</h1>
-              <table class="table">
-                <thead>
-                  <tr>
-                    <th>ID</th>
-                    <th>Nombre</th>
-                    <th>DNI</th>
-                    <th>Mail</th>
-                    <th>etc</th>
-                    <th>etc</th>
-                    <th>Plan</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>01</td>
-                    <td>Sam David</td>
-                    <td>57659033</td>
-                    <td>mail</td>
-                    <td>8:00AM</td>
-                    <td>3:00PM</td>
-                    <td>
-                      <button>Editar</button>
-                      <button>Eliminar</button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>02</td>
-                    <td>Balbina Kherr</td>
-                    <td>45673489</td>
-                    <td>mail</td>
-                    <td>9:00AM</td>
-                    <td>4:00PM</td>
-                    <td>
-                      <button>Editar</button>
-                      <button>Eliminar</button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>03</td>
-                    <td>Badan John</td>
-                    <td>76459823</td>
-                    <td>mail</td>
-                    <td>8:00AM</td>
-                    <td>3:00PM</td>
-                    <td>
-                      <button>Editar</button>
-                      <button>Eliminar</button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>04</td>
-                    <td>Sara David</td>
-                    <td>34764987</td>
-                    <td>mail</td>
-                    <td>8:00AM</td>
-                    <td>3:00PM</td>
-                    <td>
-                      <button>Editar</button>
-                      <button>Eliminar</button>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </section>
+  <div class="content-container">
+    <SidebarAdmin />
+    <div class="content">
+      <div class="head">
+        <nav class="navbar navbar-light bg-light">
+          <form class="form-inline">
+            <input
+              class="form-control mr-sm-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+              Search
+            </button>
+          </form>
+        </nav>
+      </div>
+    </div>
+    <div class="content">
+      <div class="cards">
+        <div class="card text-white bg-primary mb-1" style="max-width: 18rem">
+          <div class="card-header">Header</div>
+          <div class="card-body">
+            <h5 class="card-title">Primary card title</h5>
+            <p class="card-text">Hola</p>
+          </div>
         </div>
-      </section>
+        <div class="card text-white bg-danger mb-3" style="max-width: 18rem">
+          <div class="card-header">Header</div>
+          <div class="card-body">
+            <h5 class="card-title">Primary card title</h5>
+            <p class="card-text">Hola</p>
+          </div>
+        </div>
+        <div class="card text-white bg-success mb-3" style="max-width: 18rem">
+          <div class="card-header">Header</div>
+          <div class="card-body">
+            <h5 class="card-title">Primary card title</h5>
+            <p class="card-text">Hola</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="content">
+      <div class="tab">
+        <section class="attendance">
+          <div class="attendance-list table-container">
+            <section class="attendance">
+              <div class="attendance-list">
+                <h1>Tabla Usuarios</h1>
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Nombre</th>
+                      <th>DNI</th>
+                      <th>Mail</th>
+                      <th>etc</th>
+                      <th>etc</th>
+                      <th>Plan</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>01</td>
+                      <td>Sam David</td>
+                      <td>57659033</td>
+                      <td>mail</td>
+                      <td>8:00AM</td>
+                      <td>3:00PM</td>
+                      <!-- <td>
+                        <button>Editar</button>
+                        <button>Eliminar</button>
+                      </td> -->
+                    </tr>
+                    <tr>
+                      <td>02</td>
+                      <td>Balbina Kherr</td>
+                      <td>45673489</td>
+                      <td>mail</td>
+                      <td>9:00AM</td>
+                      <td>4:00PM</td>
+                      <!-- <td>
+                        <button>Editar</button>
+                        <button>Eliminar</button>
+                      </td> -->
+                    </tr>
+                    <tr>
+                      <td>03</td>
+                      <td>Badan John</td>
+                      <td>76459823</td>
+                      <td>mail</td>
+                      <td>8:00AM</td>
+                      <td>3:00PM</td>
+                      <!-- <td>
+                        <button>Editar</button>
+                        <button>Eliminar</button>
+                      </td> -->
+                    </tr>
+                    <tr>
+                      <td>04</td>
+                      <td>Sara David</td>
+                      <td>34764987</td>
+                      <td>mail</td>
+                      <td>8:00AM</td>
+                      <td>3:00PM</td>
+                      <!-- <td>
+                        <button>Editar</button>
+                        <button>Eliminar</button>
+                      </td> -->
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </section>
+          </div>
+        </section>
 
-      <section class="attendance">
-        <div class="attendance-list table-container">
-          <section class="attendance">
-            <div class="attendance-list">
-              <h1><center>tabla Gimnasios</center></h1>
-              <table class="table">
-                <thead>
-                  <tr>
-                    <th>ID</th>
-                    <th>Nombre</th>
-                    <th>dni</th>
-                    <th>Mail</th>
-                    <th>Etc</th>
-                    <th>etc</th>
-                    <th>Estado</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>05</td>
-                    <td>Jane Smith</td>
-                    <td>12345678</td>
-                    <td>mail</td>
-                    <td>10:00AM</td>
-                    <td>5:00PM</td>
-                    <td><button>Activo</button></td>
-                  </tr>
-                  <tr>
-                    <td>06</td>
-                    <td>John Doe</td>
-                    <td>87654321</td>
-                    <td>mail</td>
-                    <td>11:00AM</td>
-                    <td>6:00PM</td>
-                    <td><button>Activo</button></td>
-                  </tr>
-                  <!-- Agrega más filas si es necesario -->
-                </tbody>
-              </table>
-            </div>
-          </section>
-        </div>
-      </section>
+        <section class="attendance">
+          <div class="attendance-list table-container">
+            <section class="attendance">
+              <div class="attendance-list">
+                <h1><center>tabla Gimnasios</center></h1>
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Nombre</th>
+                      <th>dni</th>
+                      <th>Mail</th>
+                      <th>Etc</th>
+                      <th>etc</th>
+                      <th>Estado</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>05</td>
+                      <td>Jane Smith</td>
+                      <td>12345678</td>
+                      <td>mail</td>
+                      <td>10:00AM</td>
+                      <td>5:00PM</td>
+                      <!-- <td><button>Activo</button></td> -->
+                    </tr>
+                    <tr>
+                      <td>06</td>
+                      <td>John Doe</td>
+                      <td>87654321</td>
+                      <td>mail</td>
+                      <td>11:00AM</td>
+                      <td>6:00PM</td>
+                      <!-- <td><button>Activo</button></td> -->
+                    </tr>
+                    <!-- Agrega más filas si es necesario -->
+                  </tbody>
+                </table>
+              </div>
+            </section>
+          </div>
+        </section>
 
-      <section class="attendance">
-        <div class="attendance-list table-container">
-          <section class="attendance">
-            <div class="attendance-list">
-              <h1>tabla Miembros</h1>
-              <table class="table">
-                <thead>
-                  <tr>
-                    <th>ID</th>
-                    <th>Nombre</th>
-                    <th>Ubicación</th>
-                    <th>Mail</th>
-                    <th>Foro</th>
-                    <th>etc</th>
-                    <th>Estado</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>05</td>
-                    <td>Jane Smith</td>
-                    <td>12345678</td>
-                    <td>mail</td>
-                    <td>10:00AM</td>
-                    <td>5:00PM</td>
-                    <td><button>Activo</button></td>
-                  </tr>
-                  <tr>
-                    <td>06</td>
-                    <td>John Doe</td>
-                    <td>87654321</td>
-                    <td>mail</td>
-                    <td>11:00AM</td>
-                    <td>6:00PM</td>
-                    <td><button>Activo</button>
-                    </td>
-                  </tr>
-                  <!-- Agrega más filas si es necesario -->
-                </tbody>
-              </table>
-            </div>
-          </section>
-        </div>
-      </section>
+        <section class="attendance">
+          <div class="attendance-list table-container">
+            <section class="attendance">
+              <div class="attendance-list">
+                <h1><center>tabla Pagos</center></h1>
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Nombre</th>
+                      <th>Ubicación</th>
+                      <th>Mail</th>
+                      <th>Foro</th>
+                      <th>etc</th>
+                      <th>Estado</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>05</td>
+                      <td>Jane Smith</td>
+                      <td>12345678</td>
+                      <td>mail</td>
+                      <td>10:00AM</td>
+                      <td>5:00PM</td>
+                      <!-- <td><button>Activo</button></td> -->
+                    </tr>
+                    <tr>
+                      <td>06</td>
+                      <td>John Doe</td>
+                      <td>87654321</td>
+                      <td>mail</td>
+                      <td>11:00AM</td>
+                      <td>6:00PM</td>
+                      <!-- <td><button>Activo</button></td> -->
+                    </tr>
+                    <!-- Agrega más filas si es necesario -->
+                  </tbody>
+                </table>
+              </div>
+            </section>
+          </div>
+        </section>
+        <section class="attendance">
+          <div class="attendance-list table-container">
+            <section class="attendance">
+              <div class="attendance-list">
+                <h1><center>tabla Partners</center></h1>
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Nombre</th>
+                      <th>dni</th>
+                      <th>Mail</th>
+                      <th>Etc</th>
+                      <th>etc</th>
+                      <th>Estado</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>05</td>
+                      <td>Jane Smith</td>
+                      <td>12345678</td>
+                      <td>mail</td>
+                      <td>10:00AM</td>
+                      <td>5:00PM</td>
+                      <!-- <td><button>Activo</button></td> -->
+                    </tr>
+                    <tr>
+                      <td>06</td>
+                      <td>John Doe</td>
+                      <td>87654321</td>
+                      <td>mail</td>
+                      <td>11:00AM</td>
+                      <td>6:00PM</td>
+                      <!-- <td><button>Activo</button></td> -->
+                    </tr>
+                    <!-- Agrega más filas si es necesario -->
+                  </tbody>
+                </table>
+              </div>
+            </section>
+          </div>
+        </section>
+      </div>
     </div>
   </div>
 </template>
 <script>
-import HeaderAdmin from "@/components/layout/headers/HeaderAdmin.vue";
+import SidebarAdmin from "@/components/layout/sidebars/SidebarAdmin.vue";
 export default {
   components() {
-    HeaderAdmin;
+    SidebarAdmin;
   },
-  components: { HeaderAdmin },
+  components: { SidebarAdmin },
 };
 </script>
 <style>
+.head{
+  margin-left: 25px;
+  width: 100%;
+}
+.cards {
+  display: flex;
+  flex-direction: row; /* Cambiar de "column" a "row" */
+  width: 85%;
+  align-items: none;
+}
 .tab {
   display: flex;
   width: 85%;
@@ -187,7 +281,14 @@ export default {
 
 .content {
   flex-grow: 1;
+  margin-left: 90px; /* Ancho del sidebar */
   padding: 20px;
+  position: relative;
+}
+.content-container {
+  margin-left: 110px;
+  padding: 20px;
+  transition: all 0.3s;
 }
 
 .attendance {
@@ -281,6 +382,5 @@ table thead tr {
     width: 100%;
     max-width: 100%;
   }
-
 }
 </style>
