4f1633fc7e5840a292d213dfe14b01fa71895f11
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-07-05 21:33:34 -0500

    customer

--- src/components/layout/sidebars/SidebarCustomer.vue
+++ src/components/layout/sidebars/SidebarCustomer.vue
@@ -115,40 +115,6 @@
           </body>
           <div class="modal-footer">
             <button
-              class="btn btn-primary"
-              data-bs-target="#exampleModalToggle2"
-              data-bs-toggle="modal"
-              data-bs-dismiss="modal"
-            >
-              Siguiente
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div
-      class="modal fade"
-      id="exampleModalToggle2"
-      aria-hidden="true"
-      aria-labelledby="exampleModalToggleLabel2"
-      tabindex="-1"
-    >
-      <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalToggleLabel2">Mi Perfil</h5>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div class="modal-body">
-            Seguro que quieres modificar estos datos.
-          </div>
-          <div class="modal-footer">
-            <button
               type="button"
               class="btn btn-secondary"
               data-bs-dismiss="modal"
