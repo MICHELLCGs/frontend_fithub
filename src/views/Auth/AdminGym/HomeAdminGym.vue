8e5491929602d1c6eda7cc749b59a94741ede727
anyelinash <anyelina.sosa@tecsup.edu.pe>
2023-07-05 21:33:40 -0500

    headeradmingym:]

--- src/views/Auth/AdminGym/HomeAdminGym.vue
+++ src/views/Auth/AdminGym/HomeAdminGym.vue
@@ -1,27 +1,24 @@
 <template>
   <SidebarAdminGym />
+  <HeaderAdminGym/>
   <div class="home_content">
-    <div class="text"> Home Content </div>
-  </div>
+</div>
 </template>
-
 
 <script>
 import SidebarAdminGym from "@/components/layout/sidebars/SidebarAdminGym.vue";
+import HeaderAdminGym from "@/components/layout/headers/HeaderGym.vue";
+
 export default {
-  components() {
-    SidebarAdminGym;
-  },
-  components: { SidebarAdminGym },
+  components: {
+    SidebarAdminGym,
+    HeaderAdminGym
+  }
 };
+
 </script>
 
-
 <style>
-/*
-Home Content
-*/
-
 .home_content {
   position: absolute;
   height: 100%;
@@ -30,15 +27,6 @@ Home Content
   transition: all 0.5s ease;
   flex-grow: 1;
   overflow-y: auto;
-}
-
-.home_content .text {
-  font-size: 25px;
-  font-weight: 500;
-  color: #474D89;
-  margin: 12px;
-  flex-grow: 1;
-  overflow-y: auto
 }
 
 .sidebar.active~.home_content {
