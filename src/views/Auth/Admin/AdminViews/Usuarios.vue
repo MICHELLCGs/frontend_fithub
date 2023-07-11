0d98da0e0f7a29f86688a211fc35233850ea637b
BillyQuispe <carlos.quispe.v@tecsup.edu.pe>
2023-07-11 01:20:18 -0500

    rud

--- src/views/Auth/Admin/AdminViews/Usuarios.vue
+++ src/views/Auth/Admin/AdminViews/Usuarios.vue
@@ -1,139 +1,143 @@
 <template>
-  <SidebarAdmin />
-  <div class="home_content">
-    <div class="create-button">
-      <button class="btn btn-dark butcreate" @click="createRow">Crear Registro</button>
-    </div>
-    <main role="main" class="container tab">
-      <div class="row">
-        <div class="col-12">
-          <h1>Tabla Usuarios</h1>
-          <div class="table-responsive">
-            <table class="table table-bordered" style="margin-bottom: 100px">
-              <thead>
-                <tr>
-                  <th>ID</th>
-                  <th>NOMBRE</th>
-                  <th>APELLIDOS</th>
-                  <th>DNI</th>
-                  <th>MAIL</th>
-                  <th>ACCIONES</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="row in rows" :key="row.id">
-                  <td>{{ row.id }}</td>
-                  <td>{{ row.nombre }}</td>
-                  <td>{{ row.apellidos }}</td>
-                  <td>{{ row.dni }}</td>
-                  <td>{{ row.mail }}</td>
-                  <td>
-                    <button class="btn btn-success buttab" @click="editRow(row.id)">Editar</button>
-                    <button class="btn btn-danger buttab" @click="deleteRow(row.id)">Borrar</button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+  <div>
+    <SidebarAdmin />
+    <div class="home_content">
+      <div class="create-button">
+        <button class="btn btn-dark butcreate" @click="createRow">Crear Registro</button>
+      </div>
+      <main role="main" class="container tab">
+        <div class="row">
+          <div class="col-12">
+            <h1>Tabla Usuarios</h1>
+            <div class="table-responsive">
+              <table class="table table-bordered" style="margin-bottom: 100px">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>NOMBRE</th>
+                    <th>APELLIDOS</th>
+                    <th>DNI</th>
+                    <th>MAIL</th>
+                    <th>ROL</th>
+                    <th>ACCIONES</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="row in rows" :key="row._id">
+                    <td>{{ row._id }}</td>
+                    <td>{{ row.name }}</td>
+                    <td>{{ row.lastname }}</td>
+                    <td>{{ row.dni }}</td>
+                    <td>{{ row.email }}</td>
+                    <td>{{ row.role }}</td>
+                    <td>
+                      <button class="btn btn-success buttab" @click="editRow(row._id)">Editar</button>
+                      <button class="btn btn-danger buttab" @click="deleteRow(row._id)">Borrar</button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </main>
+      <div class="modal" v-if="editingRow">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">{{ editingRow ? 'Crear Registro' : 'Editar Registro' }}</h5>
+              <button type="button" class="btn-close" @click="cancelEdit">
+                <span></span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form @submit.prevent="saveChanges">
+                <div class="form-group">
+                  <label for="id">ID</label>
+                  <input type="text" class="form-control" id="id" v-model="editingRow._id" disabled>
+                </div>
+                <div class="form-group">
+                  <label for="nombre">Nombres</label>
+                  <input type="text" class="form-control" id="nombre" v-model="editingRow.name">
+                </div>
+                <div class="form-group">
+                  <label for="apellidos">Apellidos</label>
+                  <input type="text" class="form-control" id="apellidos" v-model="editingRow.lastname">
+                </div>
+                <div class="form-group">
+                  <label for="dni">DNI</label>
+                  <input type="text" class="form-control" id="dni" v-model="editingRow.dni">
+                </div>
+                <div class="form-group">
+                  <label for="mail">Mail</label>
+                  <input type="text" class="form-control" id="mail" v-model="editingRow.email">
+                </div>
+                <div class="form-group">
+                  <label for="rol">Rol</label>
+                  <input type="text" class="form-control" id="rol" v-model="editingRow.role">
+                </div>
+                <button type="submit" class="btn btn-primary butinside">{{ editingRow ? 'Guardar Cambios' : 'Crear' }}</button>
+                <button type="button" class="btn btn-secondary butinside" @click="cancelEdit">Cancelar</button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </main>
-    <div class="modal" v-if="editingRow">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">{{ editingRow ? 'Crear Registro' : 'Editar Registro' }}</h5>
-            <button type="button" class="btn-close" @click="cancelEdit">
-              <span></span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <form @submit.prevent="saveChanges">
-              <div class="form-group">
-                <label for="id">ID</label>
-                <input type="text" class="form-control" id="id" v-model="editingRow.id" disabled>
-              </div>
-              <div class="form-group">
-                <label for="nombre">Nombres</label>
-                <input type="text" class="form-control" id="nombre" v-model="editingRow.nombre">
-              </div>
-              <div class="form-group">
-                <label for="apellidos">apellidos</label>
-                <input type="text" class="form-control" id="apellidos" v-model="editingRow.apellidos">
-              </div>
-              <div class="form-group">
-                <label for="dni">dni</label>
-                <input type="text" class="form-control" id="dni" v-model="editingRow.dni">
-              </div>
-              <div class="form-group">
-                <label for="mail">mail</label>
-                <input type="text" class="form-control" id="mail" v-model="editingRow.mail">
-              </div>
-              <button type="submit" class="btn btn-primary butinside">{{ editingRow ? 'Guardar Cambios' : 'Crear' }}</button>
-              <button type="button" class="btn btn-secondary butinside" @click="cancelEdit">Cancelar</button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
 
+    </div>
   </div>
 </template>
-
 <script>
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importa el CSS de Bootstrap
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Importa el JavaScript de Bootstrap
 
 import SidebarAdmin from "@/components/layout/sidebars/SidebarAdmin.vue";
 export default {
-  name: "Gimnasios",
+  name: "Usuarios",
   components: {
     SidebarAdmin,
   },
   data() {
     return {
-      rows: [
-        {
-          id: "1",
-          nombre: "Lucas",
-          apellidos: "Catillo",
-          dni: "45368729",
-          mail: "example@mail.com",
-        },
-        {
-          id: "2",
-          nombre: "Thalia",
-          apellidos: "example@mail.com",
-          dni: "example@mail.com",
-          mail: "example@mail.com",
-        },
-      ],
+      rows: [],
       editingRow: null,
     };
   },
+  mounted() {
+    this.fetchRows();
+  },
   methods: {
+    fetchRows() {
+      axios.get("https://api.fithub.bjrcode.com/api/register").then((response) => {
+        this.rows = response.data.data;
+      });
+    },
     editRow(id) {
-      // Obtener los datos del registro correspondiente al ID
-      // Puedes realizar una petición a tu API o trabajar con datos simulados
-      // Aquí se utiliza el array "rows" para buscar el registro
-      const rowData = this.rows.find(row => row.id === id);
+      const rowData = this.rows.find((row) => row._id === id);
       this.editingRow = { ...rowData };
     },
     deleteRow(id) {
-      // Aquí puedes realizar una petición a tu API para borrar el registro correspondiente al ID
-      // Por simplicidad, se mostrará un mensaje en la consola y se eliminará el registro del array "rows"
-      console.log("Borrando registro con ID:", id);
-      this.rows = this.rows.filter(row => row.id !== id);
+      axios.delete(`https://api.fithub.bjrcode.com/api/register${id}`).then((response) => {
+        console.log(response.data.message);
+        this.fetchRows();
+      });
     },
     saveChanges() {
-      if (this.editingRow) {
-        // Aquí puedes realizar una petición a tu API para guardar los cambios del registro existente
-        // this.editingRow contiene los datos editados del formulario
-        console.log("Guardando cambios", this.editingRow);
+      if (this.editingRow._id) {
+        axios
+          .put(`https://api.fithub.bjrcode.com/api/register${this.editingRow._id}`, this.editingRow)
+          .then((response) => {
+            console.log(response.data.message);
+            this.fetchRows();
+          });
       } else {
-        // Aquí puedes realizar una petición a tu API para crear un nuevo registro
-        // this.editingRow contiene los datos del nuevo registro
-        console.log("Creando registro", this.editingRow);
+        axios
+          .post("https://api.fithub.bjrcode.com/api/register", this.editingRow)
+          .then((response) => {
+            console.log(response.data.message);
+            this.fetchRows();
+          });
       }
       this.cancelEdit();
     },
@@ -142,11 +146,11 @@ export default {
     },
     createRow() {
       this.editingRow = {
-        id: "",
-        nombre: "",
-        Apellidos: "",
+        name: "",
+        lastname: "",
         dni: "",
-        mail: "",
+        email: "",
+        role: "",
       };
     },
   },
